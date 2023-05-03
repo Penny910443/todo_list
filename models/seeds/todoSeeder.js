bd2fd0ff5acc7230641f6ee04b7630c91caa4519
Penny910443 <g9523519@gmail.com>
2023-05-03 15:23:14 +0800

    feat: add todo seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,12 +1,13 @@
-const express = require('express')
 const mongoose = require('mongoose')
+const Todo = require('../todo')
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const app = express()
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
 
 const db = mongoose.connection
 
@@ -16,12 +17,8 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('mongodb connected!')
-})
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.listen(3000, () => {
-  console.log('App is running on port 3000.')
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
+  }
+  console.log('done')
 })
